81338b6b0f65fc719b3cc9e04c2e8717e41773e0
Brandon Neo <brandon.neo@gmail.com>
2020-11-13 16:16:11 +1100

    Create setup-wetty.sh

---
+++ setup-wetty.sh
@@ -0,0 +1,21 @@
+
+
+# commands to install yarn
+
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt-get -y install yarn
+
+# installing npm
+sudo apt-get -y install npm
+
+# updating npm for wetty
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
+#installing wetty
+sudo yarn global add wetty
+
+#running wetty on port 80
+sudo wetty -p 80
